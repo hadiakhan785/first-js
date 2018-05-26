43a6f41ca543061e38b291d4ebe0346ccbfa4606
hadiakhan785 <hadiakhan785@gmail.com>
2018-05-26 13:48:25 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+console.log ("Hi from external js");
+
+let a = 2;
+let b = 4.1;
+let c = 0;
+let d = true;
+let e = false;
+let firstName = "sarah";
+let lastName = "ahmed";
+let greeting = "ramzan mubarak";
+let carNo = "AUP-078";
+let carNo2 = "AUP-078";
+let chair = null;
+let chair2;
+
+console.log(firstName);
+console.log(lastName);
+console.log(a);
+console.log(greeting);
+console.log(carNo);
+console.log(chair);
+console.log(chair2);
+
+
+
+
+
